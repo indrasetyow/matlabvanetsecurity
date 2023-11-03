12822f384a8eb824d0445bb42428163917429826
Indra Setyo Wibowo <indra.setyo.ft19@mail.umy.ac.id>
2023-11-03 12:49:55 +0700

    03-11-2023 aodv

--- Final.m
+++ Final.m
@@ -30,7 +30,7 @@ start1 = 11;
 figure; % Membuat figure baru
 
 subplot(5, 1, 1);
-axis([-50 350 -40 120]);
+axis([-50 350 -40 1200]);
 title('Jalur PKU');
 xlabel('Data x');
 ylabel('Data y');
@@ -91,7 +91,7 @@ hold on;
 
 subplot(5, 1, 5);
 axis([-50 350 -40 120]);
-title('Jalur PKU serangan Blackhole');
+title('Jalur PKU');
 xlabel('Data x');
 ylabel('Data y');
 grid on;
@@ -116,7 +116,7 @@ for i = 1:length(Data_t)
     subplot(5, 1, 1);
     cla;
     idx = t == Data_t(i);
-    xy_array = [xy_array; x(idx) y(idx)];
+    xy_array = [ x(idx), y(idx)];
     distance1 = sqrt((xy_array(:, 1).^2) + (xy_array(:, 2).^2));
 
 
@@ -246,7 +246,11 @@ for i = 1:length(Data_t)
 
 % Plot untuk Duration
     subplot(5, 1, 2);
-    plot(traceCount(start1:i),reachableDuration(start1:i), '-', 'Color', 'red');
+    if i <= numel(traceCount) && i <= numel(reachableDuration)
+        plot(traceCount(start1:i), reachableDuration(start1:i), '-', 'Color', 'red');
+    end
+    hold on;
+    legend('mobil&taxi', 'Location', 'northwest');
     hold on;
     legend('mobil&taxi', 'Location', 'northwest');
    
@@ -325,7 +329,7 @@ for i = 1:length(Data_t)
     data_xy_angle = [x(idx), y(idx), normalized_angle(idx)];
     k = 4; % jumlah cluster menjadi 4
     
-    [idx_kmeans, C] = kmeans(data_xy_angle, k, 'Distance', 'sqeuclidean', 'Replicates', 5);
+    [idx_kmeans, C, sumd, D] = kmeans(data_xy_angle, k, 'Distance', 'sqeuclidean', 'Replicates', 5);
     
     % Menggambar hasil clustering dengan warna yang berbeda
     for cluster = 1:k
@@ -383,7 +387,6 @@ for i = 1:length(Data_t)
     legend('RSU', 'Cluster 1', 'Centroid 1', 'Cluster 2', 'Centroid 2', 'Cluster 3', 'Centroid 3', 'Cluster 4', 'Centroid 4', 'Location', 'northwest');
 
 
-    % Plot untuk Reachable dan Unreachable
     subplot(5, 1, 4);
     cla;
 %     plot(x(idx_mobil), y(idx_mobil), 'o', 'MarkerFaceColor', 'Green');
@@ -440,7 +443,6 @@ for i = 1:length(Data_t)
     end
 
 
-
     % Menghubungkan dua titik koordinat dengan garis berdasarkan nilai unik pada Data_l
     for j = 1:length(Data_l)
         idx_l = idx & strcmp(l, Data_l(j));
@@ -448,8 +450,6 @@ for i = 1:length(Data_t)
         y_l = y(idx_l);
         id_l = data.id(idx_l); % Kolom id dari data
         type_l = data.type(idx_l); % Kolom type dari data
-
-        
 
         % Menghitung jarak antara titik dengan RSU
         distance_to_rsu = sqrt((x_l - rsu_x).^2 + (y_l - rsu_y).^2);
@@ -459,11 +459,32 @@ for i = 1:length(Data_t)
         distance_to_rsu = sqrt((x - rsu_x).^2 + (y - rsu_y).^2);
         data.Distance_to_RSU = distance_to_rsu;
 
-
+%         % Menggambar garis yang menghubungkan titik dengan RSU
+%         for k = 1:length(x_l(idx_rsu))
+%             line1 = plot([x_l(idx_rsu(k)), rsu_x], [y_l(idx_rsu(k)), rsu_y], '--', 'Color', 'cyan');
+%         end
+        
+%         % Memberikan warna pada mobil & taxi ketika jarak >= 300
+%         for k = 1:size(x_l)
+%             Xi = x_l(k);
+%             Yi = y_l(k);
+%             id_i = id_l(k); % Id kendaraan
+%             type_i = type_l{k}; % Type kendaraan
+%             if Xi <= 300 && sqrt((Xi - rsu_x).^2 + (Yi - rsu_y).^2) <= 30
+%                 node_color = 'Green';
+%             elseif Yi <= 300 
+%                 node_color = 'Red';
+%             end
+            %plot(Xi, Yi, 'o', 'MarkerFaceColor', node_color);
+            %text(Xi, Yi, [' ' id_i ,  type_i], 'Color', 'black', 'FontSize', 8);
+            %text(Xi, Yi, [type_i], 'Color', 'black', 'FontSize', 8);
+%         end
     end
-    legend('RSU', 'Centroid Reachable', 'Centroid Unreachable', 'Location', 'northwest');
+    legend('RSU', 'Centroids', 'Centroids', 'Location', 'northwest');
+    %legend('mobil','taxi', 'RSU', 'Location', 'northwest');
 
-    % Plot untuk balckhole
+
+    % Plot untuk Blackhole
     subplot(5, 1, 5);
     cla;
     plot(x(idx_mobil), y(idx_mobil), 'o', 'MarkerFaceColor', 'Green');
@@ -512,18 +533,169 @@ for i = 1:length(Data_t)
         for k = 1:length(x_l(idx_rsu))
             line1 = plot([x_l(idx_rsu(k)), rsu_x], [y_l(idx_rsu(k)), rsu_y], '--', 'Color', 'cyan');
         end
-        node_numbers = 1:length(x);
-        for k = 1:length(x)
-            x_node = x(k);
-            y_node = y(k);
-            text(x_node, y_node, num2str(node_numbers(k)), 'HorizontalAlignment', 'left', 'VerticalAlignment', 'middle');
-        end
     end
     legend('mobil','taxi', 'RSU', 'Location', 'northwest');
 
+    xi = x;
+    yi = y;
+
+    % Menghitung s1
+    s1 = [xi(1), yi(1)];
     
-
-
+    % Menghitung min_d1
+    min_d1 = zeros(size(xi));  % Inisialisasi array min_d1
+    for i = 2:numel(xi)  % Mulai dari 2 karena kita menghitung dari elemen kedua
+        min_d1(i) = sqrt((xi(i) - xi(i-1))^2 + (yi(i) - yi(i-1))^2);
+    end
+    
+    % Matriks xy_array
+    t = 20; % Ganti sesuai kebutuhan
+    xy_array = zeros(t); % Membuat matriks t x t dengan semua elemen 0
+    
+    for i = 1:t
+        for j = 1:t
+            if i == j
+                xy_array(i, j) = 0;
+            else
+                xy_array(i, j) = xy_array(j, i);
+            end
+        end
+    end
+    
+    disp(xy_array);
+    
+    t = 1:20;
+    disp(t);
+    
+    status = cell(1, 20);  % Gunakan cell array untuk status
+    dist = zeros(1, 20);   % Jarak awal
+    next = ones(1, 20);    % Inisialisasi seluruh next ke 1
+    
+    status{1} = '!';  % Inisialisasi status node 1
+    dist(1) = 0;      % Inisialisasi jarak node 1
+    next(1) = 0;      % Inisialisasi node selanjutnya node 1
+    
+    for i = 2:20
+        status{i} = '?';       % Inisialisasi status node-node lain
+        dist(i) = xy_array(i, 1);  % Inisialisasi jarak awal
+    end
+    
+    flag = 0;
+    rreq_route = zeros(1, 20);  % Rute untuk RREQ
+    rreq_route_count = 0;
+    
+    for i = 2:20
+        if xy_array(1, i) == 1
+            disp([' Node 1 sends RREQ to node ' num2str(i)]);
+            rreq_route_count = rreq_route_count + 1;
+            rreq_route(rreq_route_count) = i;
+            if i == 20 && xy_array(1, i) == 1
+                flag = 1;
+            end
+        end
+    end
+    
+    disp(['Flag = ' num2str(flag)]);
+    for vert = 1:20
+        if flag == 1
+            break;
+        end
+        
+        temp = 0;
+    
+        % Menemukan node dengan status '?'
+        for i = 1:20
+            if strcmp(status{i}, '?')
+                D = dist(i);  % Salah, seharusnya D diinisialisasi di sini
+                vert = i;
+                break;
+            end
+        end
+    
+        % Memeriksa node lain dengan status '?' dan jarak minimum
+        for i = 1:20
+            if strcmp(status{i}, '?') && dist(i) < D
+                D = dist(i);
+                vert = i;
+            end
+        end
+    
+        status{vert} = '!';
+    
+        % Menghitung berapa banyak node yang statusnya '!'
+        for i = 1:20
+            if strcmp(status{i}, '!')
+                temp = temp + 1;
+            end
+        end
+    
+        if temp == 20
+            break;
+        end
+    
+        for i = 2:20
+            next(i) = 0;  % Inisialisasi node-node lainnya
+            if xy_array(vert, i) == 1 && strcmp(status{i}, '?')
+                disp([' Node ' num2str(vert) ' sends RREQ to node ' num2str(i)]);
+                rreq_route_count = rreq_route_count + 1;
+                rreq_route(rreq_route_count) = i;
+                % Perbarui jarak dan node selanjutnya jika menemukan rute yang lebih baik
+                if D + xy_array(vert, i) < dist(i)
+                    dist(i) = D + xy_array(vert, i);
+                    next(i) = vert;
+                end
+            end
+        end
+    end
+    
+    % Membuat array untuk menyimpan rute
+    route = zeros(1, 20);
+    
+    % Menemukan rute dari node 1 ke node 20
+    count = 1;
+    i = 1; % Mulai dari node 1
+    
+    while i ~= 20
+        route(count) = i;
+        
+        % Mencari node selanjutnya dalam rute
+        i = next(i);
+        
+        count = count + 1;
+        
+        if i == 0
+            disp('Tidak ditemukan rute ke node tujuan.');
+            break;
+        end
+    end
+    
+    % Menambahkan node 20 ke rute RREP
+    route(count) = 20;
+    
+    % Menampilkan rute dalam urutan yang diinginkan
+    disp('Rute yang diinginkan:');
+    for j = 1:count
+        disp(['Node ' num2str(route(j))]);
+    end
+    
+    route(count + 1) = 1;  % Menambahkan node 1 ke rute RREP
+    
+    disp(' Node 1 ');
+    for i = count + 1: -1:1
+        disp([ ' Sends message to node ' num2str(route(i))]);
+    end
+    
+    % Menampilkan rute RREQ
+    disp('Rute RREQ:');
+    for i = 1:rreq_route_count
+        disp([' Node ' num2str(rreq_route(i))]);
+    end
+    
+    % Menampilkan rute RREP
+    disp('Rute RREP:');
+    for i = count + 1:-1:1
+        disp([' Node ' num2str(route(i))]);
+    end
         
     pause(0.45);
 
